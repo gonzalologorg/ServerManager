df79cd014934c25ca11a7da00a3d96287c1033cf
Gonzalolog <gonzalolog@gmail.com>
2023-11-07 16:53:08 -0300

    Agregar archivos de proyecto.

---
+++ ServerManager/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ServerManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            label4 = new Label();
+            clientArgs = new TextBox();
+            label3 = new Label();
+            serverArgs = new TextBox();
+            openClientPath = new LinkLabel();
+            openServerPath = new LinkLabel();
+            label2 = new Label();
+            clientPath = new TextBox();
+            serverPath = new TextBox();
+            label1 = new Label();
+            openServerButton = new Button();
+            openClientButton = new Button();
+            restartServer = new Button();
+            restartClients = new Button();
+            folderChooser = new OpenFileDialog();
+            folderBrowser = new FolderBrowserDialog();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(clientArgs);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(serverArgs);
+            groupBox1.Controls.Add(openClientPath);
+            groupBox1.Controls.Add(openServerPath);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(clientPath);
+            groupBox1.Controls.Add(serverPath);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(560, 200);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Paths";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 153);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Arguments";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // clientArgs
+            // 
+            clientArgs.Location = new Point(6, 171);
+            clientArgs.Name = "clientArgs";
+            clientArgs.Size = new Size(548, 23);
+            clientArgs.TabIndex = 8;
+            clientArgs.Text = "-multirun -insecure -fullwindowsdump -nodns -nopreload";
+            clientArgs.TextChanged += clientArgs_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Arguments";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // serverArgs
+            // 
+            serverArgs.Location = new Point(6, 81);
+            serverArgs.Name = "serverArgs";
+            serverArgs.Size = new Size(548, 23);
+            serverArgs.TabIndex = 6;
+            serverArgs.Text = resources.GetString("serverArgs.Text");
+            serverArgs.TextChanged += serverArgs_TextChanged;
+            // 
+            // openClientPath
+            // 
+            openClientPath.AutoSize = true;
+            openClientPath.Location = new Point(524, 107);
+            openClientPath.Name = "openClientPath";
+            openClientPath.Size = new Size(30, 15);
+            openClientPath.TabIndex = 5;
+            openClientPath.TabStop = true;
+            openClientPath.Text = "Find";
+            openClientPath.LinkClicked += openClientPath_LinkClicked;
+            // 
+            // openServerPath
+            // 
+            openServerPath.AutoSize = true;
+            openServerPath.Location = new Point(524, 19);
+            openServerPath.Name = "openServerPath";
+            openServerPath.Size = new Size(30, 15);
+            openServerPath.TabIndex = 4;
+            openServerPath.TabStop = true;
+            openServerPath.Text = "Find";
+            openServerPath.LinkClicked += openServerPath_LinkClicked;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Game Client (Executable)";
+            // 
+            // clientPath
+            // 
+            clientPath.Location = new Point(6, 125);
+            clientPath.Name = "clientPath";
+            clientPath.Size = new Size(548, 23);
+            clientPath.TabIndex = 2;
+            // 
+            // serverPath
+            // 
+            serverPath.Location = new Point(6, 37);
+            serverPath.Name = "serverPath";
+            serverPath.Size = new Size(548, 23);
+            serverPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Dedicated Server (Folder)";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // openServerButton
+            // 
+            openServerButton.Location = new Point(12, 218);
+            openServerButton.Name = "openServerButton";
+            openServerButton.Size = new Size(275, 23);
+            openServerButton.TabIndex = 1;
+            openServerButton.Text = "Open Server";
+            openServerButton.UseVisualStyleBackColor = false;
+            openServerButton.Click += openServerButton_Click;
+            // 
+            // openClientButton
+            // 
+            openClientButton.BackColor = SystemColors.ButtonFace;
+            openClientButton.Location = new Point(297, 218);
+            openClientButton.Name = "openClientButton";
+            openClientButton.Size = new Size(275, 23);
+            openClientButton.TabIndex = 2;
+            openClientButton.Text = "Open Client";
+            openClientButton.UseVisualStyleBackColor = false;
+            openClientButton.Click += openClientButton_Click;
+            // 
+            // restartServer
+            // 
+            restartServer.Location = new Point(12, 247);
+            restartServer.Name = "restartServer";
+            restartServer.Size = new Size(275, 23);
+            restartServer.TabIndex = 3;
+            restartServer.Text = "Close Server";
+            restartServer.UseVisualStyleBackColor = true;
+            restartServer.Click += restartServer_Click;
+            // 
+            // restartClients
+            // 
+            restartClients.Location = new Point(297, 247);
+            restartClients.Name = "restartClients";
+            restartClients.Size = new Size(275, 23);
+            restartClients.TabIndex = 4;
+            restartClients.Text = "Close Clients";
+            restartClients.UseVisualStyleBackColor = true;
+            restartClients.Click += restartClients_Click;
+            // 
+            // folderChooser
+            // 
+            folderChooser.DefaultExt = "exe";
+            folderChooser.FileName = "folderChooser";
+            folderChooser.RestoreDirectory = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 281);
+            Controls.Add(restartClients);
+            Controls.Add(restartServer);
+            Controls.Add(openClientButton);
+            Controls.Add(openServerButton);
+            Controls.Add(groupBox1);
+            MaximumSize = new Size(600, 320);
+            MinimumSize = new Size(600, 320);
+            Name = "Form1";
+            Text = "Server Dashboard";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private LinkLabel openClientPath;
+        private LinkLabel openServerPath;
+        private Label label2;
+        private TextBox clientPath;
+        private TextBox serverPath;
+        private Label label1;
+        private Button openServerButton;
+        private Button openClientButton;
+        private Button restartServer;
+        private Button restartClients;
+        private OpenFileDialog folderChooser;
+        private Label label3;
+        private TextBox serverArgs;
+        private Label label4;
+        private TextBox clientArgs;
+        private FolderBrowserDialog folderBrowser;
+    }
+}
---
+++ ServerManager/Form1.cs
@@ -0,0 +1,150 @@
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerManager
+{
+    public partial class Form1 : Form
+    {
+
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        Process serverProcess;
+        List<Process> clientProcesses = new List<Process>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            serverPath.Text = Properties.Settings.Default.serverPath;
+            clientPath.Text = Properties.Settings.Default.clientPath;
+        }
+
+        private void openServerPath_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult result = folderBrowser.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                serverPath.Text = folderBrowser.SelectedPath;
+                Properties.Settings.Default.serverPath = serverPath.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void openClientPath_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult result = folderChooser.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                clientPath.Text = folderChooser.FileName;
+                Properties.Settings.Default.clientPath = clientPath.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void serverArgs_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.serverArgs = serverArgs.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void clientArgs_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.clientArgs = clientArgs.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void openServerButton_Click(object sender, EventArgs e)
+        {
+            if (serverProcess != null)
+            {
+                serverProcess.CloseMainWindow();
+                serverProcess.Close();
+            }
+
+            serverProcess = new Process();
+            serverProcess.StartInfo.Arguments = serverArgs.Text;
+            serverProcess.StartInfo.FileName = serverPath.Text + "\\srcds.exe";
+            serverProcess.EnableRaisingEvents = true;
+            serverProcess.Exited += ServerProcess_Exited;
+
+            serverProcess.Start();
+
+            openServerButton.BackColor = Color.Chartreuse;
+        }
+
+        private void ServerProcess_Exited(object? sender, EventArgs e)
+        {
+            openServerButton.BackColor = SystemColors.ButtonFace;
+        }
+
+        private void restartServer_Click(object sender, EventArgs e)
+        {
+            if (serverProcess != null)
+            {
+                serverProcess.CloseMainWindow();
+                serverProcess.Close();
+            }
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        private void openClientButton_Click(object sender, EventArgs e)
+        {
+
+            float screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            float screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            float margin = screenHeight * .1f;
+
+            string size = "-w " + MathF.Round(screenWidth * .66f).ToString() + " -h " + MathF.Round(screenHeight * .66f).ToString();
+            size += " -x " + (clientProcesses.Count * margin * 1.25f).ToString() + " -y " + (clientProcesses.Count * margin).ToString();
+
+            Process newClient = new Process();
+            newClient.StartInfo.Arguments = clientArgs.Text + " +connect " + GetLocalIPAddress() + " " + size;
+            newClient.StartInfo.FileName = clientPath.Text;
+            newClient.EnableRaisingEvents = true;
+            newClient.Exited += NewClient_Exited;
+
+            newClient.Start();
+
+            clientProcesses.Add(newClient);
+            openClientButton.BackColor = Color.Teal;
+        }
+
+        private void NewClient_Exited(object? sender, EventArgs e)
+        {
+            List<Process> processes = new List<Process>();
+            foreach (Process process in clientProcesses)
+            {
+                if (process != null)
+                    processes.Add(process);
+            }
+
+            clientProcesses = processes;
+            if (clientProcesses.Count == 0)
+            {
+                openClientButton.BackColor = SystemColors.ButtonFace;
+            }
+        }
+
+        private void restartClients_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in clientProcesses)
+            {
+                process.CloseMainWindow();
+                process.Close();
+                process.Kill();
+            }
+
+            clientProcesses.Clear();
+        }
+    }
+}
