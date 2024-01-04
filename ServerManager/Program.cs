d91d71792cdc103dcba68003c9e061c1baa9a62d
Gonzalolog <gonzalolog@gmail.com>
2024-01-04 15:50:59 -0300

    Update Program.cs

--- ServerManager/Program.cs
+++ ServerManager/Program.cs
@@ -11,7 +11,11 @@ namespace ServerManager
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            PresetManager presets = new PresetManager();
+            ScriptsManager scripts = new ScriptsManager();
+
+            Application.Run(new Form1(presets, scripts));
         }
     }
 }
