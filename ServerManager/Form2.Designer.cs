de5f8f88902e2aca4ac504952091bc8e8c36ce8c
Gonzalolog <gonzalolog@gmail.com>
2024-01-04 15:50:48 -0300

    Added dialog system

---
+++ ServerManager/Form2.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ServerManager
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            presetName = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // presetName
+            // 
+            presetName.Location = new Point(12, 31);
+            presetName.Name = "presetName";
+            presetName.Size = new Size(287, 23);
+            presetName.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(80, 60);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Ok";
+            button1.UseVisualStyleBackColor = true;
+            button1.DialogResult = DialogResult.OK;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(161, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            button2.DialogResult = DialogResult.Cancel;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Write a name for your preset/script";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(311, 90);
+            ControlBox = false;
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(presetName);
+            Name = "Form2";
+            Text = "Save";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        public TextBox presetName;
+    }
+}
